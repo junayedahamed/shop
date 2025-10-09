bd9d9b35ea69158d6014900fdba7884f70c9e0b5
junayedahamed <junayedahamed660@gmail.com>
2025-10-10 01:23:25 +0600

    feat: update ProductCard layout to use AspectRatio and improve widget structure

--- lib/src/home/ui/widget/product_card.dart
+++ lib/src/home/ui/widget/product_card.dart
@@ -18,67 +18,70 @@ class ProductCard extends StatelessWidget {
   final bool isFavorite;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      width: 320,
-      constraints: BoxConstraints(maxHeight: 450, maxWidth: 300),
-      decoration: BoxDecoration(),
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 4.0),
-          child: Stack(
-            alignment: AlignmentGeometry.topRight,
+    return AspectRatio(
+      aspectRatio: 16 / 9,
+      child: Container(
+        // height: 400,
+        // width: 320,
+        // constraints: BoxConstraints(maxHeight: 450, maxWidth: 300),
+        decoration: BoxDecoration(),
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: Stack(
+              alignment: AlignmentGeometry.topRight,
 
-            children: [
-              Container(
-                height: iconSize + 13,
-                width: iconSize + 13,
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: Center(
-                  child: IconButton(
-                    onPressed: favoritePress,
-                    icon: Icon(
-                      isFavorite ? Icons.favorite : Icons.favorite_outline,
-                      color: isFavorite ? Colors.pink : null,
-                      size: iconSize,
+              children: [
+                Container(
+                  height: iconSize + 13,
+                  width: iconSize + 13,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade200,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Center(
+                    child: IconButton(
+                      onPressed: favoritePress,
+                      icon: Icon(
+                        isFavorite ? Icons.favorite : Icons.favorite_outline,
+                        color: isFavorite ? Colors.pink : null,
+                        size: iconSize,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  spacing: 5,
-                  children: [
-                    SizedBox(height: 100, child: Placeholder()),
-                    SizedBox(height: 2),
-                    Text(name, maxLines: 1, overflow: TextOverflow.ellipsis),
-                    Text(
-                      description,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 2,
-                    ),
-                    // Spacer(flex: 2),
-                    Spacer(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("Price:$price"),
-                        IconButton(
-                          onPressed: cartPress,
-                          icon: Icon(Icons.shopping_cart, size: iconSize),
-                        ),
-                      ],
-                    ),
-                    // SizedBox(height: 10),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    spacing: 5,
+                    children: [
+                      SizedBox(height: 100, child: Placeholder()),
+                      SizedBox(height: 2),
+                      Text(name, maxLines: 1, overflow: TextOverflow.ellipsis),
+                      Text(
+                        description,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ),
+                      // Spacer(flex: 2),
+                      Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Price:$price"),
+                          IconButton(
+                            onPressed: cartPress,
+                            icon: Icon(Icons.shopping_cart, size: iconSize),
+                          ),
+                        ],
+                      ),
+                      // SizedBox(height: 10),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
